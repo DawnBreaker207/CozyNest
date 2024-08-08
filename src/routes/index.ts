140dbbf7026e7596a3531814f494c8227791ddf2
minhhv24 <hminh0555@gmail.com>
2024-08-08 13:58:30 +0700

    upload admin-page/Product-Category

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
-import ProductAddPage from '@/pages/admin/category/add/CategoryAdd'
-import ProductPage from '@/pages/admin/category/CategoryPage'
-import ProductEditPage from '@/pages/admin/category/edit/CategoryEdit'
+import AddCategoryPage from '@/pages/admin/category/add/CategoryAdd'
+import CategoryPage from '@/pages/admin/category/CategoryPage'
+import EditCategoryPage from '@/pages/admin/category/edit/CategoryEdit'
 import DashboardPage from '@/pages/admin/dashboard/page'
 import LayoutAdmin from '@/pages/admin/layout'
+import ProductAddPage from '@/pages/admin/product/add/ProductAdd'
+import ProductEditPage from '@/pages/admin/product/edit/ProductEdit'
+import ProductPage from '@/pages/admin/product/ProductPage'
 import HomePage from '@/pages/website/home/page'
 import LayoutWebsite from '@/pages/website/layout'
 import { useRoutes } from 'react-router-dom'
@@ -18,6 +21,9 @@ function App() {
       Component: LayoutAdmin, // ensure this is a component
       children: [
         { index: true, Component: DashboardPage },
+        { path: 'categories', Component: CategoryPage },
+        { path: 'categories/add', Component: AddCategoryPage },
+        { path: 'categories/:id/edit', Component: EditCategoryPage },
         { path: 'products', Component: ProductPage },
         { path: 'products/add', Component: ProductAddPage },
         { path: 'products/:id/edit', Component: ProductEditPage }
