f635f47b3b7a3d9ef9f86a0a1f042de062a2ff88
Phuong anh <nguyenthiphuonganh151104@gmail.com>
2024-08-30 22:51:37 +0700

    news+abouts+service+footer

--- src/pages/website/home/page.tsx
+++ src/pages/website/home/page.tsx
@@ -1,10 +1,16 @@
+import AboutUs from './_components/AboutUs'
 import Banner from './_components/Banner'
+import News from './_components/News'
+import Service from './_components/Service'
 
 const HomePage = () => {
   return (
-    <div>
+    <div >
       HomePage
       <Banner />
+      <News />
+      <AboutUs />
+      <Service />
     </div>
   )
 }
