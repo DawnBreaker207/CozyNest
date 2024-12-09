121c3c74516bd2c9204ff1eed2ff21f6b6b5d9fd
DawnBreaker <tunganhngo207@gmail.com>
2024-12-09 16:55:38 +0700

    feat: update UI product

--- src/pages/website/products/_components/RelatedProduct.tsx
+++ src/pages/website/products/_components/RelatedProduct.tsx
@@ -5,12 +5,14 @@ import { Cart } from '@/components/icons'
 import { IProduct } from '@/types/product'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import { Button } from 'antd'
 type Props = {
   id: string | undefined
 }
 const RelatedProduct = ({ id }: Props) => {
   const [hoveredImages, setHoveredImages] = useState({})
   const [hoveredPrices, setHoveredPrices] = useState({})
+
   const { data, isLoading } = useQuery({
     queryKey: ['RELATED_PRODUCT', id],
     queryFn: async () => {
@@ -27,21 +29,26 @@ const RelatedProduct = ({ id }: Props) => {
 
   return (
     <div className='mt-10'>
-      <h1 className='text-[#fca120] font-semibold text-[25px] mb-8'>Xem thêm sản phẩm cùng loại</h1>
+      <h2 className='text-[#fca120] font-semibold text-[25px] mb-8'>
+        Xem thêm sản phẩm cùng loại
+      </h2>
       <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center gap-5'>
-        {products.map((product: IProduct, index: number) => {
-          // Lấy biến thể đầu tiên
-          const firstVariant = product.variants?.[0]
-          const price = firstVariant?.sku_id?.price || 'N/A' // Giá từ biến thể hoặc 'N/A'
-          const discountPrice = firstVariant?.sku_id?.price_discount_percent
-            ? firstVariant.sku_id.price - (firstVariant.sku_id.price * firstVariant.sku_id.price_discount_percent) / 100
-            : price
+        {products.map((product) => {
+          console.log(product)
+
+          // Kiểm tra các variant và lấy giá trị từ sku_id
+          const firstVariant = product?.variants?.[0]
+          console.log(firstVariant)
+
+          const price = firstVariant?.sku_id?.price || 0 // Sử dụng giá mặc định là 0 nếu không có giá
+          // const priceDiscountPercent = firstVariant?.sku_id?.price_discount_percent || 0
+          // const discountedPrice = price - price * (priceDiscountPercent / 100)
 
           return (
-            <div key={index} className='group overflow-hidden hover:shadow-lg rounded-lg pb-3 bg-white'>
-              <div className='relative'>
-                <div className='flex transition-transform ease-in-out duration-500'>
-                  <a href={`/detail/${product._id}`}>
+            <div key={product._id} className='group overflow-hidden hover:shadow-lg rounded-lg pb-3'>
+              <Link to={`/detail/${product._id}`}>
+                <div className='relative'>
+                  <div className='flex transition-transform ease-in-out duration-500'>
                     <img
                       src={
                         hoveredImages[product._id] || // Ảnh hiện tại được hover
@@ -51,70 +58,26 @@ const RelatedProduct = ({ id }: Props) => {
                       alt={product?.name}
                       className='object-cover'
                     />
-                    <FaRegEye
-                      className='absolute left-[45%] top-[50%] bg-white text-[#6d6565] rounded-full size-7 md:size-8 px-1 py-[2px] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-500 hover:bg-[#444444] hover:text-white hover:border hover:border-white'
-                      title='Xem nhanh'
-                    />
-                  </a>
+                  </div>
+
+                  <FaRegEye
+                    className='absolute left-[45%] top-[50%] bg-white text-[#6d6565] rounded-full size-7 md:size-8 px-1 py-[2px] opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-500 hover:bg-[#444444] hover:text-white hover:border hover:border-white'
+                    title='Xem nhanh'
+                  />
                 </div>
-              </div>
-              <div className='mx-2 text-center space-y-2 mt-3'>
-                <h3>{product?.name}</h3>
-                <div className='flex sm:flex-row flex-col items-center justify-center gap-2'>
-                  {/* Hiển thị giá thay đổi khi hover */}
-                  <span className='text-[#FF0000] font-semibold'>
-                    {(hoveredPrices[product._id] || product?.variants?.[0]?.sku_id?.price).toLocaleString()}₫
-                  </span>
-                  {hoveredPrices[product._id] ? (
-                    <span className='text-[#878c8f] font-light line-through text-[13px]'>
-                      {product?.variants?.[0]?.sku_id?.price.toLocaleString()}₫
+
+                <div className='mx-2 text-center space-y-2 mt-3'>
+                  <h3>{product?.name}</h3>
+                  <div className='flex sm:flex-row flex-col items-center justify-center gap-2'>
+                    {/* Hiển thị giá thay đổi khi hover */}
+                    <span className='text-[#FF0000] font-semibold'>
+                      {(hoveredPrices[product._id] || product?.variants?.[0]?.sku_id?.price).toLocaleString()}₫
                     </span>
-                  ) : null}
+                  </div>
+                  <Button >xem chi tiết</Button>
                 </div>
-                <div className='flex space-x-4'>
-                  {product.variants.map((variant, idx) => {
-                    const value = variant.option_value_id.value
-                    const bgColor =
-                      value === 'Nâu' ? 'bg-[#A0522D]' : value === 'Màu Tự Nhiên' ? 'bg-[#F5DEB3]' : 'bg-gray-200'
 
-                    const isSelected = hoveredImages[product._id]
-                      ? hoveredImages[product._id] === variant.sku_id.image?.[0]
-                      : idx === 0 // Mặc định chọn màu đầu nếu chưa hover
-
-                    return (
-                      <div
-                        key={idx}
-                        className={`w-6 h-6 rounded-full ${bgColor} cursor-pointer`}
-                        title={value}
-                        style={{
-                          outline: isSelected ? '2px solid black' : 'none', // Hiển thị viền nếu được chọn
-                          outlineOffset: '3px'
-                        }}
-                        onMouseEnter={() => {
-                          setHoveredImages((prev) => ({
-                            ...prev,
-                            [product._id]: variant.sku_id.image?.[0] || '' // Cập nhật ảnh khi hover
-                          }))
-                          setHoveredPrices((prev) => ({
-                            ...prev,
-                            [product._id]: variant.sku_id.price || null // Cập nhật giá khi hover
-                          }))
-                        }}
-                        onClick={() => {
-                          setHoveredImages((prev) => ({
-                            ...prev,
-                            [product._id]: variant.sku_id.image?.[0] // Duy trì trạng thái hover sau khi click
-                          }))
-                          setHoveredPrices((prev) => ({
-                            ...prev,
-                            [product._id]: variant.sku_id.price // Duy trì giá sau khi click
-                          }))
-                        }}
-                      />
-                    )
-                  })}
-                </div>
-              </div>
+              </Link>
             </div>
           )
         })}
